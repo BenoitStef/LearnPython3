b089463a975be377372a84ac24ea95c452aa5032
Ebn <benoit.stef@gmx.ch>
2019-02-21 00:00:58 +0100

    DEVEL: add ex5 - variables & print again

---
+++ chapter1/ex5.py
@@ -0,0 +1,18 @@
+myName = 'Ben Stef'
+myAge = 32
+myHeight = 172 # in cm
+myWeight = 68 # in kg
+myEyes = 'green'
+myTeeth = 'yellow'
+myHair = 'brown'
+
+print(f"let's talk about {myName}.")
+print(f"He's {myHeight} cm tall.")
+print(f"He's {myWeight} kh heavy")
+print("actually that's not too heavy")
+print(f"He's got {myEyes} eyes and {myHair} hair.")
+print(f"His teeth are usually {myTeeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total = myAge + myHeight + myHeight
+print(f"If I add {myAge}, {myHeight}, and {myWeight} I get {total}")
