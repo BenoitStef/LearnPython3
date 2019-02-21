fe9cfe02a643b861ce8b6d0614c6b567ba3fafba
stef_b <benoit.stef@psi.ch>
2019-02-21 17:49:23 +0100

    DEVEL: Add ex10 & ex11, scanf <=> input()

---
+++ chapter1/ex10.py
@@ -0,0 +1,17 @@
+#Exercice 10 from Learn python 3 the hard way
+#p.65 Escape Sequences
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list
+\t* CAt food
+\t* Fishies
+\t* CAtnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
---
+++ chapter2/ex11.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Asking Questions - Learn Python 3 the Hard way - page 68
+#-------------------------------------------------------------------------------
+
+#Here we let a white space for the answer that will be passing to var page
+#with input function, similar to a scanf
+print("How old are you?", end=' ')
+age = input()
+
+print("How tall you are?", end=' ')
+height = input()
+
+print("How much do you weigh?", end=' ')
+weight =input()
+
+print("So, you are", age, "old", height, "tall", weight, "heavy")
