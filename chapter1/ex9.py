75f2c0fd565c7b5bef77db2da65456a1369714d7
stef_b <benoit.stef@psi.ch>
2019-02-21 15:02:11 +0100

    DEVEL: 5/7/8/9 exercises done still printing

---
+++ chapter1/ex9.py
@@ -0,0 +1,13 @@
+#string def to variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#string with return to line
+months = " Jan\nFeb\nMar"
+#display both variable defined above
+print("here are the days:", days)
+print("here are the months.", months)
+#block text
+print("""
+Hey how are you?
+Ahaha.
+well shut up!
+""")
